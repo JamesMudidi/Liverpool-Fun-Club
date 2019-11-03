8ba36dada79654a533bff9508009b9d1d78387b7
James Mudidi <mudidi.jimmy@gmail.com>
2019-11-03 11:18:07 +0300

    make the map resposive

--- src/About/about.js
+++ src/About/about.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "../Components/header";
 import Footer from "../Components/footer";
-import Map from "../Components/map"
+import Map2 from "../Components/map"
 import "./about.scss";
 
 const About = () => {
@@ -34,48 +34,48 @@ const About = () => {
                 match viewing and have a bond as a family.
               </p>
               <br />
-              <Map />
+              <Map2 />
             </div>
           </div>
           <div className="col-sm-2"></div>
-                    <div className="row py-5">
-                        <div className="col-sm-2"></div>
-                        <div className="col-sm-8">
-                            <h3>
-                                <strong>Become a member today and get exclusive benefits</strong>
-                            </h3>
-                            <br />
-                            <p>
-                                <h4>
-                                    <strong>Membership Registration</strong>
-                                </h4>
-                                <strong>To join OLSCUG,</strong> you must fulfill the following:<br />
-                                <ol>
-                                    <li>You will need a monthly subscription fee of UShs 10,000.</li>
-                                    <li>Send your your information to us and <strong>You Will Never Walk Alone</strong>.</li>
-                                </ol>
-                            </p>
-                            <br />
-                            <p>
-                                <h4>
-                                    <strong>Membership Benefits</strong>
-                                </h4>
-                                <strong>As a member of OLSCUG,</strong> you have access to the following benefits:<br />
-                                <ol>
-                                    <li>
-                                        Priority entry to match viewings at Arena Sports Bar and lounge
+          <div className="row py-5">
+            <div className="col-sm-2"></div>
+            <div className="col-sm-8">
+              <h3>
+                <strong>Become a member today and get exclusive benefits</strong>
+              </h3>
+              <br />
+              <p>
+                <h4>
+                  <strong>Membership Registration</strong>
+                </h4>
+                <strong>To join OLSCUG,</strong> you must fulfill the following:<br />
+                <ol>
+                  <li>You will need a monthly subscription fee of UShs 10,000.</li>
+                  <li>Send your your information to us and <strong>You Will Never Walk Alone</strong>.</li>
+                </ol>
+              </p>
+              <br />
+              <p>
+                <h4>
+                  <strong>Membership Benefits</strong>
+                </h4>
+                <strong>As a member of OLSCUG,</strong> you have access to the following benefits:<br />
+                <ol>
+                  <li>
+                    Priority entry to match viewings at Arena Sports Bar and lounge
                                         <ul>
-                                            <i><li>Subject to a game-by-game agreement between OLSCUG and Arena Sports Bar and lounge</li></i>
-                                            <i><li>Priority entry does NOT cover guests of members</li></i>
-                                        </ul>
-                                    </li>
-                                    <li>Access to OLSCUG member-only events</li>
-                                </ol>
-                            </p>
-                            <br />
-                        </div>
-                    </div>
-                    <div className="col-sm-2"></div>
+                      <i><li>Subject to a game-by-game agreement between OLSCUG and Arena Sports Bar and lounge</li></i>
+                      <i><li>Priority entry does NOT cover guests of members</li></i>
+                    </ul>
+                  </li>
+                  <li>Access to OLSCUG member-only events</li>
+                </ol>
+              </p>
+              <br />
+            </div>
+          </div>
+          <div className="col-sm-2"></div>
         </Container>
       </div>
       <Footer />
---
+++ src/Components/map2.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import MapGL, {NavigationControl, Marker, Popup} from 'react-map-gl';
+import { Icon } from 'semantic-ui-react';
+const TOKEN = 'pk.eyJ1IjoibXVkaWRpIiwiYSI6ImNrMmhyY2w2NTEybGQzbXA1NGI3NzBiZGIifQ._Ombuao3cir9FFgWgE4fYg';
+const navStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px'
+};
+
+const markerList=[
+    {lat: 0.339597, 
+    long: 32.587132,
+    info: 10}
+    ];
+export default class Map2 extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 0.339597,  
+        longitude: 32.587132,
+        zoom: 15,
+        bearing: 0,
+        pitch: 0,
+        width: "50vw",
+        height: "70vh",
+      },
+      popupInfo: null
+    };
+  }
+
+renderPopup(index){
+    return this.state.popupInfo && (
+      <Popup tipSize={5}
+        anchor="bottom-right"
+        longitude={markerList[index].long}
+        latitude={markerList[index].lat}
+        onMouseLeave={() => this.setState({popupInfo: null})}
+        closeOnClick={true}>
+        <p>Available beds:{markerList[index].info}</p>
+      </Popup>
+    )
+  }
+
+render() {
+    const {viewport} = this.state;
+return (
+      <MapGL
+        {...viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+        mapStyle="mapbox://styles/mapbox/streets-v10"
+        mapboxApiAccessToken={TOKEN} >
+        <div className="nav" style={navStyle}>
+          <NavigationControl onViewportChange={(viewport) => this.setState({viewport})}/>
+              {markerList.map((marker, index)=>{
+                 return(<div key={index} > <Marker  longitude={marker.long} latitude={marker.lat}>
+                    <Icon name='Arena' size='big' onMouseEnter={()=>this.setState({popupInfo: true})} onMouseLeave={()=>this.setState({popupInfo: null})}/>
+                    </Marker> {this.renderPopup(index)}</div> );
+              }
+               ) }
+       </div>
+      </MapGL>
+    );
+  }
+}
