0208536d0114823321019fe06a249fb332c0cb02
Nelson Mwirumubi <mwinel@live.com>
2019-11-03 22:32:17 +0300

    Add route transition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import posed, { PoseGroup } from "react-pose";
 import Home from "./Home/home";
 import Contact from "./Contact/contact";
 import Certificate from "./Certificate/certificate";
@@ -7,26 +8,32 @@ import About from "./About/about";
 import GreenCampaign from "./GreenCampaign/green";
 import NotFound from "./404";
 
+const RouteContainer = posed.div({
+  enter: { opacity: 1, delay: 100, beforeChildren: true },
+  exit: { opacity: 0 }
+});
+
 export default function App() {
   return (
     <Router>
-      <div>
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/certificate" component={Certificate} />
-          <Route path="/about" component={About} />
-          <Route path="/green-campaign" component={GreenCampaign} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
+      <Route
+        render={({ location }) => (
+          <div>
+            <PoseGroup>
+              <RouteContainer key={location.pathname}>
+                <Switch location={location}>
+                  <Route exact path="/" component={Home} />
+                  <Route path="/contact" component={Contact} />
+                  <Route path="/certificate" component={Certificate} />
+                  <Route path="/about" component={About} />
+                  <Route path="/green-campaign" component={GreenCampaign} />
+                  <Route component={NotFound} />
+                </Switch>
+              </RouteContainer>
+            </PoseGroup>
+          </div>
+        )}
+      />
     </Router>
   );
 }
