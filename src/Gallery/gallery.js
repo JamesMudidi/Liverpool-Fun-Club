2dbce830e50bf6e184f6ef8f2ca2703fb75e4ec0
James Mudidi <mudidi.jimmy@gmail.com>
2019-11-05 23:40:31 +0300

    add sample images

--- src/Gallery/gallery.js
+++ src/Gallery/gallery.js
@@ -5,11 +5,47 @@ import "./gallery.scss";
 import DztImageGalleryComponent from 'reactjs-image-gallery'
 
 const Gallery = () => {
-    const data = [{
-        url: '',
-        title: '',
-        thumbUrl: '',
-    },
+    const data = [
+        {
+            url: 'images/anfield.jpg',
+            title: 'Anfield',
+            thumbUrl: 'images/anfield.jpg',
+        },
+        {
+            url: 'images/fans.jpg',
+            title: 'OSCU',
+            thumbUrl: 'images/fans.jpg',
+        },
+        {
+            url: 'images/player-celebrations.png',
+            title: 'Saleh',
+            thumbUrl: 'images/player-celebrations.png',
+        },
+        {
+            url: 'images/green-enviroment.png',
+            title: 'Environment',
+            thumbUrl: 'images/green-enviroment.png',
+        },
+        {
+            url: 'images/lfc-team-women.jpeg',
+            title: 'Women Team',
+            thumbUrl: 'images/lfc-team-women.jpeg',
+        },
+        {
+            url: 'images/inside_anfield.jpg',
+            title: 'Inside Anfield',
+            thumbUrl: 'images/inside_anfield.jpg',
+        },
+        {
+            url: 'images/squad.jpg',
+            title: 'Squad',
+            thumbUrl: 'images/squad.jpg',
+        },
+        {
+            url: 'images/cl-celebration.png',
+            title: 'Champions legue celebration',
+            thumbUrl: 'images/cl-celebration.png',
+        },
     ];
     return (
         <div>
